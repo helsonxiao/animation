85f5917502d9e83432e6ca9d7d3d046fa599625d
helsonxiao <helsonxiao@gmail.com>
2019-10-26 18:48:45 +0800

    first commit

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,38 @@
 <template>
 	<view class="content">
-		<image class="logo" src="/static/logo.png"></image>
-		<view class="text-area">
-			<text class="title">{{title}}</text>
-		</view>
+		<uni-icons :type="iconType" size="30" @click="switchStar"></uni-icons>
+    <view :class="starClass" @click="star"></view>
+    <!-- <view class="heart"></view> -->
 	</view>
 </template>
 
 <script>
+  import { uniIcons } from '@dcloudio/uni-ui';
 	export default {
+    components: {
+      uniIcons,
+    },
 		data() {
 			return {
-				title: 'Hello'
+				iconType: 'star',
+        starClass: 'fave',
 			}
 		},
 		onLoad() {
 
 		},
 		methods: {
-
-		}
+      switchStar() {
+        if (this.iconType === 'star') {
+          this.iconType = 'star-filled';
+          return;
+        }
+        this.iconType = 'star';
+      },
+      star() {
+        this.starClass += " active";
+      }
+		},
 	}
 </script>
 
@@ -31,22 +44,40 @@
 		justify-content: center;
 	}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 200rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-
-	.text-area {
-		display: flex;
-		justify-content: center;
-	}
-
-	.title {
-		font-size: 36rpx;
-		color: #8f8f94;
-	}
+  .fave {
+    background: url(../../static/twitter_fave.png) no-repeat;
+    background-position: 0 0;
+    width: 70px;
+    height: 50px;
+    cursor: pointer;
+  }
+  .fave.active {
+    background-position: -3519px 0;
+    transition: background 0.8s steps(55);
+    /* transition: background 0.8s linear; */
+  }
+  
+  .heart {
+    width: 100px;
+    height: 100px;
+    background: url(https://cssanimation.rocks/images/posts/steps/heart.png) no-repeat;
+    background-position: 0 0;
+    animation: fave-heart 2s steps(28) infinite;
+    cursor: pointer;
+  }
+  .heart:hover {
+    background-position: -2800px 0;
+    transition: background 1s steps(28);
+  }
+  @keyframes fave-heart {
+    0% {
+      background-position: -2800px 0;
+    }
+    50% {
+      background-position: 0 0;
+    }
+    100% {
+      background-position: -2800px 0;
+    }
+  }
 </style>
